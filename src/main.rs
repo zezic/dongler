16ca53bddc5a25ff169227b9a4891c7ad613eaf9
surrealistic creature <i.don.t.know.where@gmail.com>
2022-02-03 13:54:19 +0300

    first steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+	send_message();
+}
+
+fn send_message() -> io::Result<()> {
+	loop {
+    	let mut buffer = String::new();
+    	let mut stdin = io::stdin(); // We get `Stdin` here.
+    	stdin.read_line(&mut buffer)?;
+    	println!("user say: {}", buffer);
+	}
+
+    Ok(())
 }
